7f65db61563b1bcb1983b0acce8dc95d66778d35
Tommassino <batsup@gmail.com>
2016-05-01 13:53:42 +0200

    voice improvements

--- modules/Voice.py
+++ modules/Voice.py
@@ -1,10 +1,11 @@
 import discord
 import re
 import asyncio
+import glob
+import os.path
 
 class Module(object):
 	def __init__(self, wrapper):
-		self.voice = None
 		self.wrapper = wrapper
 		self.messageHooks = {
 			re.compile('^!join .*'): JoinVoice('!join',self),
@@ -17,14 +18,15 @@ class JoinVoice(object):
 		self.module=module
 
 	def __call__(self, msg, mtc, cli):
-		
+		if cli.voice:
+			yield from cli.voice.disconnect()
 		channel_name=msg.content[len(self.cmd):].strip()
 		check = lambda c: c.name == channel_name and c.type == discord.ChannelType.voice
 		channel = discord.utils.find(check,msg.server.channels)
 		if channel is None:
 			yield from cli.send_message(msg.channel,"Invalid channel name")
 			return
-		self.module.voice = yield from cli.join_voice_channel(channel)
+		yield from cli.join_voice_channel(channel)
 
 class PlaySound(object):
 	def __init__(self, cmd, module, wrapper, play_cost):
@@ -33,15 +35,27 @@ class PlaySound(object):
 		self.player=None
 		self.wrapper = wrapper
 		self.play_cost = play_cost
+		self.extensions = ["wav","mp3"]
 
 	def __call__(self, msg, mtc, cli):
-		global voiceChannel
 		coins = self.wrapper.money.get_money(msg.author)
 		if coins < self.play_cost:
-			yield from cli.send_message(msg.channel,"Fuck off, not enough lil bitch credit!")
+			yield from cli.send_message(msg.channel, "Fuck off, not enough lil bitch credit!")
+			return
+		if not cli.voice:
+			yield from cli.send_message(msg.channel, "Cant play if im not in a voice channel, use !join to tell me where to play.")
+			return
+		files = glob.glob("./audio/"+msg.content[len(self.cmd):].strip()+".*")
+		if len(files)==0:
+			yield from cli.send_message(msg.channel, "Cant find that audio file, sorry man... :(")
+			return
+		file = files[0] #could throw warning if there are more than one
+		extension = os.path.splitext(file)[1]
+		if extension not in self.extensions:
+			yield from cli.send_message(msg.channel, "The audio file found has a unsupported extension, sorry man... :(")
 			return
 		if not self.player or self.player.is_done():
+			print("Playing audio file {0}".format(file))
 			self.wrapper.money.add_money(msg.author, -self.play_cost)
-			file = "audio/"+msg.content[len(self.cmd):].strip()+".wav"
-			self.player = self.module.voice.create_ffmpeg_player(file)
+			self.player = cli.voice.create_ffmpeg_player(file)
 			self.player.start()
